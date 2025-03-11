e078cdcdc564f8ab728ca7b00cbf929b7bcf6423
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2025-03-11 19:11:22 +0100

    Corrected option name

--- src/external/reebhantun.cmake
+++ src/external/reebhantun.cmake
@@ -1,40 +1,40 @@
 # Opzione per abilitare il download
-option(ALLOW_DOWNLOAD_REEBHANTUN "Allow download of ReebHanTun source" ON)
+option(MESHLAB_ALLOW_DOWNLOAD_REEBHANTUN "Allow download of ReebHanTun source" ON)
 
-if(ALLOW_DOWNLOAD_REEBHANTUN)
+if(MESHLAB_ALLOW_DOWNLOAD_REEBHANTUN)
 	# Directory di destinazione per il download
 	set(REEBHANTUN_DIR "${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/ReebHanTun-main")
 	set(REEBHANTUN_CHECK "${REEBHANTUN_DIR}/vcgtest/main.cpp")  # Usa il file main.cpp come controllo
 	set(ANN_CHECK "${REEBHANTUN_DIR}/ann/ANN/ANN.cpp")  # Usa il file ANN.cpp come controllo
-
-	# Controlla se il file di verifica esiste
+	# Check if the folder exists
 	if (NOT EXISTS ${REEBHANTUN_CHECK})
-		# Link per scaricare lo zip da GitHub
+		# define the link to download the zip from GitHub
 		set(REEBHANTUN_LINK
 			https://github.com/andr3w1699/ReebHanTun/archive/refs/heads/main.zip)
 
-		# Funzione di download e decompressione
+		# call function to download and unzip
 		download_and_unzip(
 			NAME "ReebHanTun"
 			LINK ${REEBHANTUN_LINK}
 			DIR ${MESHLAB_EXTERNAL_DOWNLOAD_DIR})
 
-		# Verifica se il download è riuscito
+		# verify if the download was successful
 		if (NOT download_and_unzip_SUCCESS)
 			message(STATUS "- ReebHanTun - download failed.")
 		endif()
-	endif()	
+	endif()
+	# download ANN library	
 	if (NOT EXISTS ${ANN_CHECK})
-			# Link per scaricare lo zip da GitHub
+			# Link for download from the original repository
 			set(ANN_LINK
 				https://github.com/michaelquigley/ann/archive/refs/heads/master.zip)
-			# Funzione di download e decompressione
+			# call function to download and unzip
 			download_and_unzip(
 				NAME "ANN"
 				LINK ${ANN_LINK}
 				DIR ${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/ReebHanTun-main)
 			file(RENAME ${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/ReebHanTun-main/ann-master  ${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/ReebHanTun-main/ann)
-			# Verifica se il download è riuscito
+			# if the download was successful
 			if (NOT download_and_unzip_SUCCESS)
 				message(STATUS "- ANN - download failed.")
 			endif()
